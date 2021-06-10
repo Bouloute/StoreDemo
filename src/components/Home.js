adb313e612d23a02fb3b5361701fdf27a8c73d7f
Bouloute <annaick1995@gmail.com>
2021-06-09 22:19:09 -0400

    banner on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,10 @@ class Home extends Component {
     render() {
         return (
             <div>
-                HOME
+                <img
+                    src="https://cdna4.zoeysite.com/Adzpo594RQGDpLcjBynL1z/cache=expiry:31536000//auto_image/compress/https://s3.amazonaws.com/zcom-media/sites/a0iE000000Q9y3wIAB/media/mediamanager/Free_shipping_banner_12_24edit_1_.jpg"
+                    style={{width: '100%'}}
+                />
             </div>
         );
     }
